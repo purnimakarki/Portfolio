00d8a6bc002af1f2d1b02be808256660174425d1
Purnima Karki <PurnimaKarki44@gmail.com>
2024-12-09 16:54:15 +0545

    first portfolio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,33 +1,130 @@
-import { RiGithubFill, RiLinkedinBoxFill } from '@remixicon/react';
-import  {useRef} from 'react';
+import {
+  RiGithubFill,
+  RiLinkedinBoxFill,
+  RiMailFill,
+  RiMapPin2Fill,
+  RiPhoneFill,
+} from "@remixicon/react";
+import { useRef } from "react";
 
 const Contact = () => {
   const contactRef = useRef(null);
-  return(
-    <section className='py-16' id="contact" ref={contactRef}>
-      <div className='px-4 text-center'>
-        <h2 className='mb-8 text-3xl font-medium lg:text-4xl'>Contact</h2>
-        <p className='mb-4 text-lg lg:text-xl'>
-          feel free to reach  out to me via email:{""}
-          <a href='mailto:purnimakarki44@gmail.com' className= "border-b">
-            purnimakarki44@gmail.com
-          </a>
-        </p>
-        <div className='mt-8 flex justify-center space-x-6'>
-          <a href="https://www.linkedin.com/in/purnima-karki-210578324/" target='_blank'
-          rel="noopener noreferrer" className='contact-icon' 
-          aria-label='Visit my LinkedIn Profile'>
-            <RiLinkedinBoxFill className='text-3xl'/>
-          </a>
-          <a href="https://github.com/purnimakarki" target='_blank'
-          rel="noopener noreferrer" className='contact-icon' 
-          aria-label='Visit my GitHub Profile'>
-            <RiGithubFill className='text-3xl'/>
-          </a>
-          
+
+  return (
+    <section className="py-16" id="contact" ref={contactRef}>
+      <div className="max-w-6xl mx-auto px-4 lg:flex lg:justify-between lg:items-start">
+        {/* Address, Phone, Email with Icons */}
+        <div className="lg:w-1/2 mb-8 lg:mb-0">
+          <h2 className="mb-8 text-3xl font-medium lg:text-4xl">Contact Us</h2>
+          <div className="mb-6 flex items-start space-x-4">
+            <RiMapPin2Fill className="text-2xl text-blue-600" />
+            <div>
+              <p>Pepsicola,</p>
+              <p>Kathmandu, Nepal</p>
+            </div>
+          </div>
+          <div className="mb-6 flex items-start space-x-4">
+            <RiPhoneFill className="text-2xl text-blue-600" />
+            <div>
+              <p>9866577254</p>
+            </div>
+          </div>
+          <div className="mb-6 flex items-start space-x-4">
+            <RiMailFill className="text-2xl text-blue-600" />
+            <div>
+              <p>
+                <a className="text-white hover:underline">
+                  purnimakarki44@gmail.com
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className="mt-8 flex space-x-6">
+            <a
+              href="https://www.linkedin.com/in/purnima-karki-210578324/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="contact-icon"
+              aria-label="Visit my LinkedIn Profile"
+            >
+              <RiLinkedinBoxFill className="text-3xl text-blue-600" />
+            </a>
+            <a
+              href="https://github.com/purnimakarki"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="contact-icon"
+              aria-label="Visit my GitHub Profile"
+            >
+              <RiGithubFill className="text-3xl text-blue-600" />
+            </a>
+          </div>
+        </div>
+
+        {/* Contact Form */}
+        <div className="lg:w-1/2 p-6 rounded-lg shadow-md">
+          <h3 className="text-2xl font-medium mb-4">Send Message</h3>
+          <form
+            action="https://formcarry.com/s/or-GklrvQzm"
+            method="POST"
+            accept-charset="UTF-8"
+          >
+            <div className="mb-4">
+              <label
+                htmlFor="fullName"
+                className="block text-sm font-medium mb-2 text-b"
+              >
+                Full Name
+              </label>
+              <input
+                type="text"
+                id="fullName"
+                name="fullName"
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+                placeholder="Your Full Name"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-sm font-medium mb-2">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+                placeholder="Your Email Address"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="message"
+                className="block text-sm font-medium mb-2"
+              >
+                Type Your Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                rows="4"
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+                placeholder="Your Message"
+                required
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600"
+            >
+              Send
+            </button>
+          </form>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
+
 export default Contact;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,79 +20,85 @@ const Hero = () => {
         duration: 0.8,
         stagger: 0.15,
       })
-      .from(
-        ".hero-subheading",
-        {
-          opacity: 0,
-          y: 50,
-          scale: 0.95,
-          duration: 0.8,
-        },
-        "-=0.4",
-      )
-      .from(
-        ".hero-text",{
-          opacity: 0,
-          y: 30,
-          duration: 0.7,
-        },
-        "-=0.4",
-      )
-      .from(
-      ".her-btn",{
-        oppacity: 0,
-        scale:0.9,
-        duration: 0.7,
-      },
-      "-=0.3",
-      )
-      .from(
-        ".hero-img",{
-          opacity: 0,
-          y: 100,
-          scale:0.9,
-          duration: 0.8,
-        }
-        , "-=0.5",
-      );
+        .from(
+          ".hero-subheading",
+          {
+            opacity: 0,
+            y: 50,
+            scale: 0.95,
+            duration: 0.8,
+          },
+          "-=0.4"
+        )
+        .from(
+          ".hero-text",
+          {
+            opacity: 0,
+            y: 30,
+            duration: 0.7,
+          },
+          "-=0.4"
+        )
+        .from(
+          ".hero-btn",
+          {
+            opacity: 0,
+            scale: 0.9,
+            duration: 0.7,
+          },
+          "-=0.3"
+        )
+        .from(
+          ".hero-img",
+          {
+            opacity: 0,
+            y: 100,
+            scale: 0.9,
+            duration: 0.8,
+          },
+          "-=0.5"
+        );
+    }, heroref);
 
-    }, heroref)
     return () => ctx.revert();
-  },[]);
+  }, []);
 
   return (
     <section
-      className="flex min-h-screen flex-col items-center justify-start gap-4 lg:gap-6"
+      className="min-h-screen flex items-center justify-center px-4 lg:px-8"
       ref={heroref}
     >
-      <div className="mt-8 lg:mt-20">
-        <h1 className="hero-title text-4xl uppercase lg:text-7xl">
-          {PROFILE.name}
-        </h1>
-        <h2 className="hero-subheading bg-gradient-to-b from-pink-200 to-purple-300 bg-clip-text text-center text-2xl tracking-tighter text-transparent">
-          {PROFILE.role}
-        </h2>
+      <div className="flex flex-col lg:flex-row items-center gap-8 lg:gap-16 max-w-7xl w-full">
+        <div className="lg:w-1/2">
+          <img
+            src={Purnima}
+            alt={PROFILE.name}
+            width={400}
+            height="300"
+            className="hero-img rounded-3xl border-purple-300/20 p-1 w-full max-w-md mx-auto"
+          />
+        </div>
+
+        <div className="lg:w-1/2 flex flex-col items-center lg:items-start gap-4">
+          <h1 className="hero-title text-4xl uppercase lg:text-7xl">
+            {PROFILE.name}
+          </h1>
+          <h2 className="hero-subheading bg-gradient-to-b from-pink-200 to-purple-300 bg-clip-text text-center lg:text-left text-2xl tracking-tighter text-transparent">
+            {PROFILE.role}
+          </h2>
+          <p className="hero-text text-center lg:text-left text-xl tracking-tighter lg:text-2xl">
+            {PROFILE.subheading}
+          </p>
+          <a
+  href="/public/Purnima_cv.pdf"
+  download="Purnima_cv.pdf"
+  className="hero-btn flex gap-1 rounded-full border border-pink-200/50 px-3 tracking-tighter hover:bg-pink-200/10"
+>
+  <span>Download Resume</span>
+  <RiArrowRightUpLine />
+</a>
+        </div>
       </div>
-      <p className="hero-text max-w-2xl p-2 text-center text-xl tracking-tighter lg:text-2xl">
-        {PROFILE.subheading}
-      </p>
-      <a
-        href="/Purnima_Karki_Chhetri.pdf"
-        target="_blank"
-        rel="noopener noreferrer"
-        download
-        className="hero-btn mb-6 flex gap-1 rounded-full border border-pink-200/50 px-3 tracking-tighter"
-      >
-        <span>download Resume</span>
-        <RiArrowRightUpLine />
-      </a>
-      <img
-        src={Purnima}
-        alt={PROFILE.name}
-        width={400}
-        height="300"
-        className="hero-img rounded-3xl border-purple-300/20 p-1"
-      />
     </section>
   );
 };
